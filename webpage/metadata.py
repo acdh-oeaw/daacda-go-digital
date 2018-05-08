a14c6320b46fe2be7779e07b332f7440357a071f
aureon249 <Martin.Kirnbauer@oeaw.ac.at>
2018-05-08 14:01:00 +0200

    updated metadata.py fix #1

--- webpage/metadata.py
+++ webpage/metadata.py
@@ -2,14 +2,12 @@
 # (by default) in the base.html and index.html
 
 PROJECT_METADATA = {
-    'title': 'Django Base Project',
-    'author': 'Peter Andorfer',
-    'subtitle': 'A django project to bootstrap further web-app developments',
-    'description': 'This django projects includes some generic applications (webpage, vocabs, entities, bib)',
-    'github': 'https://github.com/acdh-oeaw/daacda',
-    'purpose_de': 'Ziel von Django Base Project ist die Schaffung einer einheitlichen und generischen Grundlage für Web Applikationen.',
-    'purpose_en': 'The purpose of Django Base Project is to bootstrap web developments.',
-    'version': '0.0.1',
-    'matomo_id': 'provide some',
-    'matomo_url': '//piwik.apollo.arz.oeaw.ac.at/'
+    'title': 'DAACDA',
+    'author': 'Peter Andorfer, Martin Kirnbauer',
+    'subtitle': 'Downed Allied Air Crew Database Austria',
+    'description': 'Das Projekt DAACDA umfasst die Sammlung, Auswertung und Darstellung von Daten und Informationen zum Schicksal amerikanischer und britischer Flugzeugbesatzungen, die zwischen 1939 und 1945 über dem heutigen Österreich zum Absturz kamen, in Gefangenschaft gerieten, Opfer von Verbrechen wurden und/oder zu Tode kamen.',
+    'github': 'https://github.com/acdh-oeaw/daacda-go-digital',
+    'purpose_de': 'Die für diese Webapplikation vorgenommene vollständige Sammlung von Daten zu allen Flugzeugabstürzen und davon betroffenen Besatzungen lässt erstmals ein umfassendes Bild der Schicksale abgeschossener Flieger im heutigen Österreich entstehen, das von einer vernetzten Betrachtung von Quellen, Daten und Informationen aus US-, britischen, deutschen und österreichischen Archiven gespeist wird.',
+    'purpose_en': 'The complete collection of data on all aircraft crashes and affected crews created for this web application for the first time provides a comprehensive picture of the fate of downed airmen in today´s Austria, based on a networked view of sources, data and information from US, British, German and Austrian Archives.',
+    'version': '2.0.0',
 }
