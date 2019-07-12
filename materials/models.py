7d5e0dfae19d1c41d96895fac6622ae8f64560c0
Peter Andorfer <Peter.Andorfer@oeaw.ac.at>
2019-07-12 11:19:57 +0200

    added materials.models.Gedenkzeichen

--- materials/models.py
+++ materials/models.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import User
 from ckeditor.fields import RichTextField
 from ckeditor_uploader.fields import RichTextUploadingField
 
-from entities.models import Bomber, Person, WarCrimeCase
+from entities.models import Bomber, Person, Place, WarCrimeCase, DATE_ACCURACY
 from vocabs.models import SkosConcept
 
 
@@ -16,6 +16,74 @@ BOOLEAN_CHOICES = (
     (True, "Yes"),
     (False, "No")
 )
+
+
+class Gedenkzeichen(models.Model):
+    """ provide some information """
+
+    name = models.CharField(
+        max_length=250, blank=True,
+        help_text="Name des Gedenkzeichens"
+    )
+    start_date = models.DateField(
+        blank=True, null=True,
+        verbose_name="Start date",
+        help_text="The day the Gedenkzeichen started"
+    )
+    end_date = models.DateField(
+        blank=True, null=True,
+        verbose_name="End date",
+        help_text="The day the Gedenkzeichen ended"
+    )
+    date_accuracy_start = models.CharField(
+        default="Y", max_length=3, choices=DATE_ACCURACY,
+        blank=True, null=True,
+        verbose_name="Date accuracy start date",
+        help_text="An information stating whether the given start date is accurate"
+    )
+    date_accuracy_end = models.CharField(
+        default="Y", max_length=3, choices=DATE_ACCURACY,
+        blank=True, null=True,
+        verbose_name="Date accuracy end date",
+        help_text="An information stating whether the given end date is accurate"
+    )
+    object_type = models.ForeignKey(
+        SkosConcept, blank=True, null=True,
+        related_name='is_gedenkzeichentyp_of',
+        on_delete=models.SET_NULL,
+        verbose_name="The Gedenkzeichens type",
+        help_text="The Gedenkzeichens type",
+    )
+    location = models.ForeignKey(
+        Place, blank=True, null=True, on_delete=models.SET_NULL,
+        help_text="The location of the Gedenkzeichen",
+        related_name='location_of_gedenkzeichen',
+    )
+    related_person = models.ForeignKey(
+        Person, blank=True, null=True,
+        verbose_name="Related person",
+        help_text="Persons related to this Gedenkzeichen relation",
+        related_name="gd_has_related_persons",
+        on_delete=models.SET_NULL
+    )
+    related_bomber = models.ForeignKey(
+        Bomber, blank=True, null=True,
+        verbose_name="Related bomber",
+        help_text="Persons related to this Gedenkzeichen relation",
+        related_name="gd_has_related_bombers",
+        on_delete=models.SET_NULL
+    )
+    description = RichTextUploadingField(
+        blank=True, null=True,
+        verbose_name="Description",
+        help_text="Description",
+    )
+    legacy_id = models.CharField(max_length=300, blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return "{}".format(self.id)
 
 
 class UserContribution(models.Model):
