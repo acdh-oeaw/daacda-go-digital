98ae7a01d1a4a855e893254d64eb9ae6ccddf13b
csae8092 <peter.andorfer@oeaw.ac.at>
2022-02-14 10:06:57 +0100

    clean up of requirements.txt

--- daacda/settings/base.py
+++ daacda/settings/base.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 
 import os
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
-
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(os.path.join(__file__, '../'))))
 
